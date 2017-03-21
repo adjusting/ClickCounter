8c3997c0971d6dbba89adf054b1ceaf0cfe96297
Justin Gareau <jgareau@evernote.com>
2017-03-20 23:36:49 -0700

    Add action to button

--- ClickCounter/ViewController.swift
+++ ClickCounter/ViewController.swift
@@ -29,6 +29,7 @@ class ViewController: UIViewController {
         button.setTitleColor(UIColor.blue, for: .normal)
         self.view.addSubview(button)
 
+        button.addTarget(self, action: #selector(ViewController.incrementCount), for: UIControlEvents.touchUpInside)
     }
 
     func incrementCount() {
